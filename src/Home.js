ac533692e2131576368b7437b6cb97ab6ce6ad33
EdASalazar <edasalazar123@gmail.com>
2023-03-12 11:21:54 -0700

    lesson 5

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,18 @@
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log('hello, ninjas', e)
+  }
+
+  const handleClickAgain = (name, e) => {
+    console.log('hello' + name, e.target);
+  }
+
   return ( 
     <div className="home">
       <h2>Home Page</h2>
-      <p>Get Home</p>
+      <button onClick={ handleClick }>Click Me</button>
+      <button onClick={(e) => handleClickAgain(' mario', e)}>Click me Again</button>
     </div>
    );
 }
