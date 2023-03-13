92d68325bdf4cb0902c1131133b9bdfd1130fc96
EdASalazar <edasalazar123@gmail.com>
2023-03-12 20:43:11 -0700

    20

--- src/Home.js
+++ src/Home.js
@@ -2,26 +2,14 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data);
-        setIsPending(false);
-      })
-    }, 1000);
-  }, [])
+
 
   return (
     <div className="home">
-      {isPending && <div>Loading...</div> }
-      {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
     </div>
   );
 }
